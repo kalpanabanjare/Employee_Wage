ddfe5af2d12fc926b10f39f91defeb84459af238
kalpanabanjare <kalpanabanjare99@gmail.com>
2022-03-25 14:55:08 +0530

    Added: Employee is fullTime or partTime

--- Employee_Wage/Employee_Wage/Program.cs
+++ Employee_Wage/Employee_Wage/Program.cs
@@ -1,15 +1,26 @@
 ﻿Console.WriteLine("Welcome to Employee Wage Computation Program.");
-//UC2
+//UC3
 int Present = 1; //Constant
+int Fulltime = 1;
 int Emp_per_rate = 20;
 int EmpHr = 0;
 int Emp_wage = 0;
-Random random = new Random(); //Computation
-int EmpStatus = random.Next(0, 2);
+Random random_attendance = new Random(); //Computation
+int EmpStatus = random_attendance.Next(0, 2);
+Random random_Time = new Random(); // creating second object
+int Emp_Time = random_Time.Next(0, 2);
 if (EmpStatus == Present)
 {
-    Console.WriteLine("Employee is Present");
-    EmpHr = 8;
+    if (Emp_Time == Fulltime)
+    {
+        Console.WriteLine("Employee is Present");
+        EmpHr = 8;
+    }
+    else
+    {
+        Console.WriteLine("Employee is Present");
+        EmpHr = 4;
+    }
 }
 else
 {
